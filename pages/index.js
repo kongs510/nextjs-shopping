2084f73d79c4e2e85458263fe778137e2c6c3de9
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-13 13:13:41 +0900

    add layout list page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
+import axios from "axios";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Header } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
 export default function Home() {
+  const [list, setList] = useState([]);
+
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>next.js 듀토리얼</h1>
-      <p>step by step</p>
+      <Head>
+        <title>kongs510 | shpping mall</title>
+      </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <ItemList list={list.slice(9)} />
     </div>
-  )
+  );
 }
