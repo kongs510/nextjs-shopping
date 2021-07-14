e1e9a5f656383a4af56f543c34c0e56505aef1c4
DESKTOP-7TUJ74H\comin <yoursitup90@gmail.com>
2021-07-14 23:00:27 +0900

    add loading page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Header } from "semantic-ui-react";
 import ItemList from "../src/component/ItemList";
+
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
 
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
@@ -13,6 +15,7 @@ export default function Home() {
     axios.get(API_URL).then((res) => {
       console.log(res.data);
       setList(res.data);
+      setIsLoading(false)
     });
   }
 
@@ -25,14 +28,25 @@ export default function Home() {
       <Head>
         <title>kongs510 | shpping mall</title>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <ItemList list={list.slice(9)} />
+      {isloading && (
+        <div class="ui segment" style={{ padding:"300px 0" }}>
+          <div class="ui active inverted dimmer">
+            <div class="ui medium text loader">Loading</div>
+          </div>
+        </div>
+      )}
+      {!isloading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
