eedbd33ae4895847834cc17ca36d6ae22d9d323c
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-15 14:55:50 +0900

    add router

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,24 +1,27 @@
 import { Menu } from "semantic-ui-react";
+import router, { useRouter } from "next/router";
 
 export default function Gnb() {
-    const activeItem = "home";   
-    return(
-        <Menu inverted>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-        //   onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-        //   onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-        //   onClick={this.handleItemClick}
-        />
-      </Menu>
-    );
-};
+  const activeItem = "home";
+  const router = useRouter();
+  console.log(router);
+
+  function goLink(e, data) {
+    if (data.name === "home") {
+      router.push("/");
+    } else if (data.name === "about") {
+      router.push("/about");
+    }
+  }
+
+  return (
+    <Menu inverted>
+      <Menu.Item name="home" active={activeItem === "home"} onClick={goLink} />
+      <Menu.Item
+        name="about"
+        active={activeItem === "about"}
+        onClick={goLink}
+      />
+    </Menu>
+  );
+}
