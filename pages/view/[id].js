88336c2381fa7d41553b6e0659b87f87235b6cca
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-16 15:03:46 +0900

    add env config

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -4,28 +4,30 @@ import { useEffect, useState } from "react";
 import Item from "../../src/component/Item";
 import Head from "next/head";
 
-export default function post({ item }) {
-
-    return (
-        <>
-            <Head>
-                <title>{item.name}</title>
-                <meta name="description" content={item.description}></meta>
-            </Head>
-            {item && <Item item={item} />}
-        </>
-    )
-};
+export default function post({ item, name }) {
+  return (
+    <>
+      <Head>
+        <title>{item.name}</title>
+        <meta name="description" content={item.description}></meta>
+      </Head>
+      {name} 환경입니다.
+      {item && <Item item={item} />}
+    </>
+  );
+}
 
 export async function getServerSideProps(context) {
-    const id = context.params.id;
-    const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-    const res = await axios.get(apiUrl);
-    const data = res.data;
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
 
-    return {
-        props: {
-            item: data,
-        },
-    };
-} []
+  return {
+    props: {
+      item: data,
+      name: process.env.name,
+    },
+  };
+}
+[];
