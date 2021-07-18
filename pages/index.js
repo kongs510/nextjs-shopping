1c3ce413dde13aa6ed434ed9e30d7e3201cbddf3
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-18 11:47:17 +0900

    Change server-side rendering to static files

--- pages/index.js
+++ pages/index.js
@@ -4,54 +4,37 @@ import { useEffect, useState } from "react";
 import { Header } from "semantic-ui-react";
 import ItemList from "../src/component/ItemList";
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isloading, setIsLoading] = useState(true);
-
-  // const API_URL =
-  //   "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      console.log(res.data);
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>kongs510 | shpping mall</title>
         <meta name="description" content="kongs 의 블로그 연습용입니다."></meta>
       </Head>
-      {isloading && (
-        <div
-          className="ui segment"
-          style={{ padding: "300px 0", boxShadow: "none", border: "none" }}
-        >
-          <div className="ui active inverted dimmer">
-            <div className="ui medium text loader">Loading</div>
-          </div>
-        </div>
-      )}
-      {!isloading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
+}
+[];
